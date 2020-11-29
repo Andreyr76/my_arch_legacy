c45bca44c90978158cd0ed9e1bf275c4b8081f15
Andreyr76 <rabotbiz@gmail.com>
2020-11-29 20:11:12 +0300

    Finish

--- my_arch_legacy3.sh
+++ my_arch_legacy3.sh
@@ -17,12 +17,12 @@ sudo pacman -Rs xfburn orage parole ristretto
 echo 'Установка базовых программ и пакетов'
 sudo pacman -S reflector firefox firefox-i18n-ru ufw f2fs-tools dosfstools ntfs-3g alsa-lib alsa-utils file-roller p7zip unrar gvfs aspell-ru pulseaudio pavucontrol youtube-dl peek --noconfirm
 
-sudo pacman -S chromium flameshot veracrypt vlc freemind filezilla gimp libreoffice libreoffice-fresh-ru neofetch qbittorrent screenfetch plank telegram-desktop gparted evince keepassxc atom nano vim neovim featherpad fbreader audacious git ktouch klavaro clamtk mc htop tmux unrar zip ark gnome-disk-utility gedit uget smplayer smtube pycharm-community-edition --noconfirm
+sudo pacman -S chromium flameshot vlc freemind filezilla gimp libreoffice libreoffice-fresh-ru neofetch qbittorrent screenfetch plank telegram-desktop gparted evince keepassxc atom nano vim neovim featherpad fbreader audacious git ktouch klavaro clamtk mc htop tmux unrar zip ark gnome-disk-utility gedit uget smplayer smtube --noconfirm
 
 gpg --keyserver keys.gnupg.net --recv-keys FC918B335044912E 
 
 yay -Syy
-yay -S xflux hunspell-ru pamac-aur-git conky-lua timeshift tusk vscodium-bin figma-linux xorg-xkill teamviewer etcher-bin garuda-settings-manager-git dropbox ttf-symbola ttf-clear-sans --noconfirm
+yay -S xflux hunspell-ru pamac-aur-git conky-lua timeshift tusk xorg-xkill etcher-bin garuda-settings-manager-git dropbox ttf-symbola ttf-clear-sans --noconfirm
 
 echo 'Качаем и устанавливаем настройки Xfce'
 # Чтобы сделать копию ваших настоек перейдите в домашнюю директорию ~/username
@@ -44,6 +44,11 @@ sudo tar -xzf themes.tar.gz -C ~/
 wget https://github.com/Andreyr76/my_arch_legacy/raw/main/attach/icons.tar.gz
 sudo tar -xzf icons.tar.gz -C ~/
 
+echo 'Шрифты'
+wget https://github.com/Andreyr76/my_arch_uefi/raw/main/attach/fonts.tar.gz
+sudo tar -xzf fonts.tar.gz -C ~/
+sudo fc-cache -f -v
+
 yay -S x-arc-shadow papirus-maia-icon-theme-git breeze-default-cursor-theme breeze-obsidian-cursor-theme xcursor-breeze-serie-obsidian --noconfirm
 
 sudo pacman -S capitaine-cursors --noconfirm
@@ -59,7 +64,7 @@ sudo mv -f ~/downloads/bg.jpg /usr/share/backgrounds/xfce/bg.jpg
 
 echo 'Conky & bashrc'
 wget https://github.com/Andreyr76/my_arch_legacy/raw/main/attach/bash.tar.gz
-tar -xzf bash.tar.gz -C ~/
+sudo tar -xzf bash.tar.gz -C ~/
 
 echo 'Подключаем zRam'
 yay -S zramswap --noconfirm
